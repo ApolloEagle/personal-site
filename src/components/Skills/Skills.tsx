5fe31aaa7bd3f4b06cc45b490d391996c157d2f9
Blake Reimer <blake.reimer@Blake-Reimer-MacBook-Pro.local>
2022-11-17 01:15:01 +0000

    styling updates

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,8 +4,8 @@ import skills from "../../data";
 
 const Skills = (): JSX.Element => {
   return (
-    <div className="flex flex-col justify-center items-center text-white bg-slate-900">
-      <div className="flex-row justify-start items-center grid grid-rows-6 grid-flow-col gap-3 lg:gap-5 md:gap-4">
+    <div className="flex flex-col justify-center items-center text-white bg-slate-900 pt-4 sm:pt-0">
+      <div className="flex-row justify-start items-center grid grid-rows-6 grid-flow-col gap-y-3 gap-x-5 lg:gap-5 md:gap-4">
         {skills.map((skill, i) => {
           return (
             <Card key={i} name={skill.name}>
